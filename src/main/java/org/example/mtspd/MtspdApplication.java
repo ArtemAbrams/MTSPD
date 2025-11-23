2690b97411caaa80c332bfda57d7efbb1c17c799
Artem Barysh <abarysh@thingsboard.io>
2025-11-23 20:35:27 +0200

    first commit

---
+++ src/main/java/org/example/mtspd/MtspdApplication.java
@@ -0,0 +1,13 @@
+package org.example.mtspd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtspdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtspdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/mtspd/MtspdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mtspd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtspdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
